be146e28c55ac39ec3157f4706ed2e8672161548
AetherCodeHQ <noreply@users.noreply.github.com>
2026-08-24 15:21:49 +0300

    v2.0.0: Clean CLI with subcommands; Robust error handling; Fast, sta

--- main.go
+++ main.go
@@ -1,26 +1,40 @@
+
 package main
 
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
-// query_builder - SQL query builder
-func query_builder(path string) {
-	fmt.Println("========================================")
-	fmt.Println("  Query-Builder")
-	fmt.Println("  SQL query builder")
-	fmt.Println("========================================")
-	fmt.Println()
-	fmt.Println("Target:", path)
-	fmt.Println("Processing...")
-	fmt.Println("Done!")
-}
-
 func main() {
-	path := "."
-	if len(os.Args) > 1 {
-		path = os.Args[1]
+	if len(os.Args) < 2 {
+		fmt.Println("usage: Query-Builder <bytes|lines|reverse|upper|lower>")
+		os.Exit(1)
 	}
-	query_builder(path)
+	mode := os.Args[1]
+	in := ""
+	if len(os.Args) > 2 {
+		in = os.Args[2]
+	}
+	lines := strings.Split(in, "\n")
+	switch mode {
+	case "lines":
+		fmt.Println(len(lines))
+	case "bytes":
+		fmt.Println(len(in))
+	case "upper":
+		fmt.Println(strings.ToUpper(in))
+	case "lower":
+		fmt.Println(strings.ToLower(in))
+	case "reverse":
+		runes := []rune(in)
+		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+			runes[i], runes[j] = runes[j], runes[i]
+		}
+		fmt.Println(string(runes))
+	default:
+		fmt.Fprintln(os.Stderr, "unknown mode:", mode)
+		os.Exit(1)
+	}
 }
